1967593055042a19488930755b090ad9b92ffbeb
SergioRuval <ruvaloser@gmail.com>
2023-03-22 19:49:20 -0600

    CHANGE env variable to define default port

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const app = require('./app');
 
-app.listen(3000);
-console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT);
+});
