101ed8b85d5769584eba3b924c199bba0115c61a
SergioRuval <ruvaloser@gmail.com>
2023-03-22 19:52:16 -0600

    CREATE jugadores.controller with template method

---
+++ src/controller/jugadores.controller.js
@@ -0,0 +1,12 @@
+const mysql = require("../mysql");
+
+exports.findJugadoresPropios = (req, res) => {
+    // Para encontrar los jugadores propios lo que tengo que hacer es pasar el id del equipo propio
+    // Esto me devolverá un listado de ids de jugadores para el equipo que necesito
+    // Con ese listado, voy obteniendo los jugadores que correspondan a dichos ids
+    // Ese listado de jugadores es el que voy a devolver en la respuesta
+}
+
+exports.findJugadoresContrarios = (req, res) => {
+    
+}
